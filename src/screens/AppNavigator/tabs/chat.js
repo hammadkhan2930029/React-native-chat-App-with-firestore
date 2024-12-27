92714e4ca6f9c670c7624f486305b4eeebd4744d
hammadkhan2930029 <hammad2930029@gmail.com>
2024-12-27 09:42:12 +0500

    Updated Code

--- src/screens/AppNavigator/tabs/chat.js
+++ src/screens/AppNavigator/tabs/chat.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 import { responsiveFontSize, responsiveWidth } from "react-native-responsive-dimensions";
-import { GiftedChat } from 'react-native-gifted-chat'
-import { useNavigation,useRoute } from "@react-navigation/native";
+import { GiftedChat,Bubble, Avatar } from 'react-native-gifted-chat'
+import { useNavigation, useRoute } from "@react-navigation/native";
 import Animated, { useAnimatedStyle, withSpring } from 'react-native-reanimated';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import firestore, { query } from '@react-native-firebase/firestore'
@@ -12,66 +12,70 @@ import firestore, { query } from '@react-native-firebase/firestore'
 const MyChat = () => {
     const route = useRoute();
     const navigation = useNavigation();
-    const { data, id } = route.params; 
+    const { data, id } = route.params;
 
     const [messagesList, setMessagesList] = useState([])
     useEffect(() => {
-       const subscriber = firestore()
-       .collection("chat")
-       .doc(id + data.userID)
-       .collection("message")
-       .orderBy("createdAt","desc")
-       subscriber.onSnapshot(querysnapshot => {
-        const allMessages = querysnapshot.docs.map(item => {
-            return {
-                ...item._data,
-                createdAt: Date.parse(new Date())
-            }
+        const subscriber = firestore()
+            .collection("chat")
+            .doc(id + data.userID)
+            .collection("message")
+            .orderBy("createdAt", "desc")
+        subscriber.onSnapshot(querysnapshot => {
+            const allMessages = querysnapshot.docs.map(item => {
+
+                return {
+                    ...item._data,
+
+
+                    createdAt: Date.parse(new Date())
+                }
+            })
+            setMessagesList(allMessages)
+
         })
-        setMessagesList(allMessages)
 
-       })
-
-       return ()=> subscriber;
+        return () => subscriber;
     }, [])
 
-    const onSend = useCallback( async (messages = []) => {
+    const onSend = useCallback(async (messages = []) => {
         const msg = messages[0]
         const myMsg = {
             ...msg,
-            sendBy :id,
-            sentTo:data.userID,
-            createdAt : Date.parse(msg.createdAt)
+            sendBy: id,
+            sentTo: data.userID,
+            createdAt: firestore.FieldValue.serverTimestamp(),
+            // createdAt : Date.parse(msg.createdAt)
         }
         setMessagesList(previousMessages =>
             GiftedChat.append(previousMessages, myMsg),
         );
         firestore()
-        .collection("chat")
-        .doc("" + id + data.userID)
-        .collection("message")
-        .add(myMsg)
+            .collection("chat")
+            .doc("" + id + data.userID)
+            .collection("message")
+            .add(myMsg)
         //-----------------
         firestore()
-        .collection("chat")
-        .doc("" + data.userID + id)
-        .collection("message")
-        .add(myMsg)
+            .collection("chat")
+            .doc("" + data.userID + id)
+            .collection("message")
+            .add(myMsg)
     }, [])
-    console.log("ID",data)
+    console.log("ID", data)
     //-------------------------------------------------
 
 
     return (
-    
 
-        <Animated.View style={{ flex: 1,backgroundColor:'#fff'}}>
-            <View style={{backgroundColor:'#fff',width:responsiveWidth(100),padding:10,alignItems:'center',flexDirection:'row',elevation:5}}>
-               <TouchableOpacity onPress={()=> navigation.goBack()}>
 
-                 <Icon name="arrow-back" size={30} color='#000' />
-               </TouchableOpacity>
-                <Text style={{color:'#000',fontSize:responsiveFontSize(2.5),fontWeight:"600",marginLeft:10}}>{data.name}</Text>
+        <Animated.View style={{ flex: 1, backgroundColor: '#fff' }}>
+            <View style={{ backgroundColor: '#fff', width: responsiveWidth(100), padding: 10, alignItems: 'center', flexDirection: 'row', elevation: 5 }}>
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+
+                    <Icon name="arrow-back" size={30} color='#000' />
+                </TouchableOpacity>
+                <Text style={{ color: '#000', fontSize: responsiveFontSize(2.5), fontWeight: "600", marginLeft: 10 }}>{data.name}</Text>
             </View>
 
             <GiftedChat
@@ -80,9 +84,30 @@ const MyChat = () => {
                 user={{
                     _id: id,
                 }}
+                renderAvatar={null}
+                renderBubble={props => {
+                    return (
+                        <Bubble
+                            {...props}
+                            wrapperStyle={{
+                                left: { // Samne wale ke msg ka style
+                                    marginVertical: 0, // Remove extra space
+                                    padding: 5,
+                                    backgroundColor: '#f0f0f0', // Customize background
+                                },
+                                right: { // Apke msg ka style
+                                    marginVertical: 0, // Remove extra space
+                                    padding: 5,
+                                    backgroundColor: '#0078FF', // Customize background
+                                },
+                            }}
+                        />
+                    );
+                }}
             />
+
         </Animated.View>
-    
+
     )
 }
 export default MyChat;
@@ -91,30 +116,4 @@ export default MyChat;
 
 
 
-// import { useRoute,useNavigation } from '@react-navigation/native';
-// import React,{useState,useEffect} from 'react';
-// import {Text,View} from 'react-native';
 
-
-
-
-// const MyChat = () => {
-//     const route = useRoute();
-//     const navigation = useNavigation()
-   
-//     const { data, id } = route.params; 
-   
-//     console.log("ID",id)
-//     console.log("Data",data)
-
-
-//     return (
-//         <View style={{flex:1}}>
-//             <Text style={{color:"#000",}}>USer ID : {id}</Text>
-
-          
-//         </View>
-//     )
-// }
-
-// export default MyChat;
